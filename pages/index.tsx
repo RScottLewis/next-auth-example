d1c38cac48d7dc0895a8d779b2ac8fe4a6cba258
RScottLewis <r.scott.lewis@gmail.com>
2023-01-27 20:07:43 -0500

    Page
    clarification

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>Dashboard Entry Point</h1>
       <p>
         This is the Wampum1st Wallet signup or sign in page{" "}
         <a href="https://wampum1st.com"></a> for authentication and security.
